6f6a5a0c27f60e8573a489bd80ec8448d977843a
Ludvig Sandh <givdul11@hotmail.se>
2026-03-07 12:32:55 +0100

    feat: add model for Exercise database table

--- app/models.py
+++ app/models.py
@@ -3,9 +3,10 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
+    exercises = db.relationship("Exercise", back_populates="user", cascade="all, delete-orphan")
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -15,3 +16,17 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.name}>"
+
+
+class Exercise(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(120), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    param1 = db.Column(db.String(80), nullable=False, default="Weight")
+    param2 = db.Column(db.String(80), nullable=False, default="Reps")
+    is_favorited = db.Column(db.Boolean, nullable=False, default=False)
+
+    user = db.relationship("User", back_populates="exercises")
+
+    def __repr__(self):
+        return f"<Exercise {self.name}>"
