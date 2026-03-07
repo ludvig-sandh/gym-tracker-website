0cbf684eb4735afeb1ed8c9345dadc66bb4422e9
Ludvig Sandh <givdul11@hotmail.se>
2026-03-07 18:01:24 +0100

    feat: add animation for sliding in exercises in /exercises page

--- app/routes.py
+++ app/routes.py
@@ -138,12 +138,14 @@ def exercises_index():
         exercises_query = exercises_query.join(Exercise.muscle_groups).filter(MuscleGroup.name == selected_muscle_group)
 
     exercises = exercises_query.order_by(Exercise.name).all()
+    delays = {exercise.id: 0.03 * index for index, exercise in enumerate(exercises)}
 
     return render_template(
         "exercises/exercises.html",
         mobile=mobile(),
         title="TRÄNINGSUTVECKLING",
         exercises=exercises,
+        delays=delays,
         muscle_groups=muscle_groups,
         selected_muscle_group=selected_muscle_group,
     )
